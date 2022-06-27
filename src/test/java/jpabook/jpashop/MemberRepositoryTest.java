29633637c93e2ceb61e7742b502883f1d5a1c55b
semanticker <semanticker@gmail.com>
2022-06-27 23:30:05 +0900

    DB 설정, 동작확인, TestCase 작성

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.*;
 
@@ -17,19 +19,21 @@ public class MemberRepositoryTest {
     MemberRepository memberRepository;
 
     @Test
+    @Transactional
+    @Rollback(value = false)
     public void testMember() throws Exception {
         //given
         Member member = new Member();
         member.setUsername("memberA");
 
         // when
-        Long id = memberRepository.save(member);
-        Member member1 = memberRepository.find(id);
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
 
         //then
-        Assertions.assertThat(member1.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(member1.getUsername()).isEqualTo(member.getUsername());
-
-
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember == member = " + (findMember == member));
     }
 }
